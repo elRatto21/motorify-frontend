135ecb4c351be56c2181cb302cd902926863d065
Niklas Trapp <niklas031712@gmail.com>
2024-05-30 11:41:46 +0200

    add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BikeListComponent } from './pages/bike-list/bike-list.component';
+import { ManufacturerListComponent } from './pages/manufacturer-list/manufacturer-list.component';
+import { AppLoginComponent } from './pages/app-login/app-login.component';
+import { appCanActivate } from './guard/app.auth.guard';
+import { AppRoles } from '../app.roles';
+import { ManufacturerEditComponent } from './pages/manufacturer-edit/manufacturer-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLoginComponent,
+  },
+  {
+    path: 'bike',
+    component: BikeListComponent,
+    pathMatch: 'full',
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Admin, AppRoles.User] },
+  },
+  {
+    path: 'manufacturer',
+    component: ManufacturerListComponent,
+    pathMatch: 'full',
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Admin, AppRoles.User] },
+  },
+  {
+    path: 'manufacturer/create',
+    component: ManufacturerEditComponent,
+    pathMatch: 'full',
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Admin] },
+  },
+  {
+    path: 'manufacturer/edit/:id',
+    component: ManufacturerEditComponent,
+    pathMatch: 'full',
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Admin] },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
