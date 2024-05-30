d17913bc44c01ba3e41743c6d066fcb8f6155197
Niklas Trapp <niklas031712@gmail.com>
2024-05-30 15:10:29 +0200

    add form to edit or create bikes

---
+++ src/app/pages/bike-edit/bike-edit.component.ts
@@ -0,0 +1,90 @@
+import { Component } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormControl,
+  UntypedFormGroup,
+} from '@angular/forms';
+import { Bike } from '../../data/bike';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BikeService } from '../../service/bike.service';
+import { ManufacturerService } from '../../service/manufacturer.service';
+import { Manufacturer } from '../../data/manufacturer';
+
+@Component({
+  selector: 'app-bike-edit',
+  templateUrl: './bike-edit.component.html',
+  styleUrl: './bike-edit.component.scss',
+})
+export class BikeEditComponent {
+  bike = new Bike();
+  manufacturerList: Manufacturer[] = [];
+  bikeTypeList: string[] = [
+    'NAKEDBIKE',
+    'SUPERSPORTS',
+    'SUPERMOTO',
+    'ENDURO',
+    'CHOPPER',
+    'TOURING',
+  ];
+  title: string = '';
+
+  public bikeForm = new UntypedFormGroup({
+    manufacturer: new UntypedFormControl(''),
+    model: new UntypedFormControl(''),
+    bikeType: new UntypedFormControl(''),
+    year: new UntypedFormControl(''),
+    horsepower: new UntypedFormControl(''),
+    weight: new UntypedFormControl(''),
+    mileage: new UntypedFormControl(''),
+  });
+
+  constructor(
+    private route: ActivatedRoute,
+    private bikeService: BikeService,
+    private manufacturerService: ManufacturerService,
+    private formBuilder: UntypedFormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (this.route.snapshot.paramMap.get('id') !== null) {
+      const id = Number.parseInt(
+        this.route.snapshot.paramMap.get('id') as string
+      );
+
+      this.bikeService.getOne(id).subscribe((obj) => {
+        this.bike = obj;
+        this.bikeForm = this.formBuilder.group(obj);
+        this.title = 'Edit Bike';
+      });
+    } else {
+      this.bikeForm = this.formBuilder.group(this.bike);
+      this.title = 'Create Bike';
+    }
+    this.manufacturerService.getList().subscribe((manufacturers) => {
+      this.manufacturerList = manufacturers;
+    });
+  }
+
+  async save(formData: any) {
+    this.bike = Object.assign(formData);
+    this.bike.username = localStorage.getItem('username') as string;
+    if (this.bike.id) {
+      this.bikeService.update(this.bike).subscribe({
+        next: () => {
+          this.back();
+        },
+      });
+    } else {
+      this.bikeService.save(this.bike).subscribe({
+        next: () => {
+          this.back();
+        },
+      });
+    }
+  }
+
+  back() {
+    this.router.navigate(['bike']);
+  }
+}
