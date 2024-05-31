7aa5d456715c3b1655bc107586efc5d4978acfd7
Niklas Trapp <niklas031712@gmail.com>
2024-05-31 08:59:54 +0200

    create structure for maintenance and expense list

---
+++ src/app/components/maintenance-list/maintenance-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-list',
+  templateUrl: './maintenance-list.component.html',
+  styleUrl: './maintenance-list.component.scss'
+})
+export class MaintenanceListComponent {
+
+}
