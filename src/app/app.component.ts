3855ab32d8ecccea02fc042d0a37c4cf16f980f4
Niklas Trapp <niklas031712@gmail.com>
2024-05-31 11:23:55 +0200

    fix linter problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppAuthService } from './service/app.auth.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { AppAuthService } from './service/app.auth.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'motorify-frontend';
 
   useralias = '';
