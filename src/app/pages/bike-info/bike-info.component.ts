d4f1decf99d2e6a2fadd1444ae79fc9221fa24a1
Niklas Trapp <niklas031712@gmail.com>
2024-05-31 09:00:06 +0200

    implement bike info view

---
+++ src/app/pages/bike-info/bike-info.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { BikeService } from '../../service/bike.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Bike } from '../../data/bike';
+
+@Component({
+  selector: 'app-bike-info',
+  templateUrl: './bike-info.component.html',
+  styleUrl: './bike-info.component.scss',
+})
+export class BikeInfoComponent {
+  constructor(
+    private bikeService: BikeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  bike: Bike = new Bike();
+
+  ngOnInit(): void {
+    if (this.route.snapshot.paramMap.get('id') !== null) {
+      const id = Number.parseInt(
+        this.route.snapshot.paramMap.get('id') as string
+      );
+      this.bikeService.getOne(id).subscribe((bike) => {
+        this.bike = bike;
+      });
+    }
+  }
+
+  edit(id: number) {
+    this.router.navigate(['bike/edit', id]);
+  }
+}
